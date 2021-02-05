6327debf88db174a66cb63a1d97529f8a9864f7a
HBX <1448151237@qq.com>
2021-02-05 23:46:51 +0800

    three-daliy

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const px2rem = require('postcss-px2rem');
+
+module.exports = {
+  publicPath: './',
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          px2rem({
+            remUnit: 37.5,
+          }),
+        ],
+      },
+    },
+  },
+  outputDir: 'docs',
+};
